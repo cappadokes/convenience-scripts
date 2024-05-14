8a3b0d7f30757953041226466f82fb8e970cca36
Panagiotis Xanthopoulos <panos0511@gmail.com>
2024-05-14 22:05:27 +0000

    No message

--- build-xla.sh
+++ build-xla.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <number_of_jobs>"
+    exit 1
+fi
+
+if ! [[ $1 =~ ^[0-9]+$ ]]; then
+    echo "Error: The argument must be an integer."
+    exit 1
+fi
+
+num_jobs=$1
+
 cd /workspace/xla
-bazel build --test_output=all --spawn_strategy=sandboxed //xla/service/memory_space_assignment:best_fit_repacker_prof
+bazel build --test_output=all --spawn_strategy=sandboxed //xla/service/memory_space_assignment:best_fit_repacker_prof --jobs=$num_jobs
 cd /workspace
