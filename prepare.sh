6e003ba3fb3c43d6b5209f87472cddaa10597a6f
Panagiotis Xanthopoulos <panos0511@gmail.com>
2024-05-15 15:58:40 +0000

    No message

--- prepare.sh
+++ prepare.sh
@@ -49,6 +49,8 @@ mkdir /workspace/results/capacity-experiment
 mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks
 mkdir /workspace/results/capacity-experiment/mindspore-benchmarks
 
+benchmark_types=("minimalloc" "mindspore")
+
 for benchmark_type in "${benchmark_types[@]}"
 do
     packers=("minimalloc" "tvm-hillclimb")
