2c4c92b37128ccc8179eef294803f27af144015b
Panagiotis Xanthopoulos <panos0511@gmail.com>
2024-05-15 16:16:16 +0000

    No message

--- gather-loads.sh
+++ gather-loads.sh
@@ -18,3 +18,19 @@ for file in $output_dir/*.csv; do
         rm -rf '"'$filename_no_ext'"_m_62'
     fi
 done
+
+outfiles=("/workspace/results/time-makespan/mindspore-benchmarks/xla-desc-size-heap-sim/csv-out/resnet50-out.csv" "/workspace/results/time-makespan/mindspore-benchmarks/xla-desc-size-heap-sim/csv-out/tiny_bert-out.csv")
+load_file="/workspace/results/capacity-experiment/loads/mindspore-loads.csv"
+> $load_file
+
+for file in "${outfiles[@]}"; do
+    if [ -f "$file" ]; then
+        $adapt_bin $file
+        filename_no_ext=$(basename -- "$file" .csv)
+        output=$($report_bin "$filename_no_ext.plc" | grep "Max load:" | grep at)
+        load=$(echo "$output" | awk '{print $3}')
+        echo -n "$load," >> "$load_file"
+        rm "$filename_no_ext.plc"
+        rm -rf '"'$filename_no_ext'"_m_62'
+    fi
+done
