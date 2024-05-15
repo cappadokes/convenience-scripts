0409eee1ddab843c01af295461d60b580b356064
Panagiotis Xanthopoulos <panos0511@gmail.com>
2024-05-15 13:42:41 +0000

    No message

---
+++ build-mindspore.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <number_of_jobs>"
+    exit 1
+fi
+
+if ! [[ $1 =~ ^[0-9]+$ ]]; then
+    echo "Error: The argument must be an integer."
+    exit 1
+fi
+
+num_jobs=$1
+
+cd /workspace/mindspore-wrapper
+git clone https://github.com/martinus/robin-hood-hashing.git
+cd /workspace/mindspore-wrapper/robin-hood-hashing
+cmake . -G Ninja
+ninja -j $num_jobs
+
+cd /workspace/mindspore-wrapper
+conan install . --build=missing
+cmake . -G Ninja
+ninja -j $num_jobs
