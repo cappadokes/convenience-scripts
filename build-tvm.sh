4496c08ae3fc465782a23bc7cf4c7ca533895cbc
Panagiotis Xanthopoulos <panos0511@gmail.com>
2024-05-15 00:06:43 +0000

    No message

--- build-tvm.sh
+++ build-tvm.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <number_of_jobs>"
+    exit 1
+fi
+
+if ! [[ $1 =~ ^[0-9]+$ ]]; then
+    echo "Error: The argument must be an integer."
+    exit 1
+fi
+
+num_jobs=$1
+
 cd /workspace/tvm
 
 mkdir /workspace/tvm/build
@@ -9,3 +21,9 @@ config_path=/workspace/tvm/build/config.cmake
 sed -i 's/set(BUILD_STATIC_RUNTIME OFF)/set(BUILD_STATIC_RUNTIME ON)/g' "$config_path"
 sed -i 's/set(USE_CCACHE AUTO)/set(USE_CCACHE ON)/g' "$config_path"
 sed -i 's/set(USE_LIBBACKTRACE AUTO)/set(USE_LIBBACKTRACE COMPILE)/g' "$config_path"
+
+cd /workspace/tvm/build
+cmake .. -G Ninja
+ninja -j $num_jobs
+
+cd /workspace
