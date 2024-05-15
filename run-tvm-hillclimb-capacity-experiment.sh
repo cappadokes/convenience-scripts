95ba5d9489d2d384c9946dbc5e9f9d605a658b87
Panagiotis Xanthopoulos <panos0511@gmail.com>
2024-05-15 17:08:18 +0000

    No message

--- run-tvm-hillclimb-capacity-experiment.sh
+++ run-tvm-hillclimb-capacity-experiment.sh
@@ -1,44 +1,53 @@
 #!/bin/bash
 
+bin="/workspace/tvm-wrapper/tvm_packer"
+
 loads=()
 
 while read -r number; do
     loads+=("$number")
-done < /workspace/results/capacity-experiment/loads/mindspore-loads.csv
+done < /workspace/results/capacity-experiment/loads/minimalloc-loads.csv
 
-capacities=()
+time_file="/workspace/results/capactiy-experiment/minimalloc-benchmarks/tvm-hillclimb/time/time.csv"
+> $time_file
 
-for num in "${loads[@]}"; do
-    result=$(echo "scale=2; $num * 125 / 100" | bc)
-    ceiling=$(echo "$result" | awk '{print ($0-int($0)>0)?int($0)+1:int($0)}')
-    capacities+=("$ceiling")
+echo -e "\n\nRunning TVM-hillclimb for minimalloc-benchmarks (capacity experiment data)\n\n"
+
+percentages=("125" "130" "135")
+
+for percentage in "${percentages[@]}"; do  
+    echo "Capacity=$percentage% of maximum load"
+
+    capacities=()
+
+    for num in "${loads[@]}"; do
+        result=$(echo "scale=2; $num * $percentage / 100" | bc)
+        ceiling=$(echo "$result" | awk '{print ($0-int($0)>0)?int($0)+1:int($0)}')
+        capacities+=("$ceiling")
+    done
+
+    index=0
+    for input in /workspace/benchmarks/minimalloc/*.csv; do
+        capacity=${capacities[$index]}
+
+        echo "Running with percentage $percentage and capacity $capacity"
+
+        # base_filename=$(basename "$input")
+        # filename_no_ext="${base_filename%.*}"
+        # time=$(BASE_PATH=/workspace/results/capactiy-experiment/minimalloc-benchmarks/tvm-hillclimb TRACE_NAME=$filename_no_ext-$percentage timeout 3m $bin $input hillclimb $capacity)
+        # ret=$?
+        # if [ $ret -eq 124 ]; then
+        #     echo -n "Failed," >> $time_file
+        #     if [ -f "/workspace/results/capactiy-experiment/minimalloc-benchmarks/tvm-hillclimb/csv-out/$filename_no_ext-$percentage-out.csv" ]; then
+        #         rm /workspace/results/capactiy-experiment/minimalloc-benchmarks/tvm-hillclimb/csv-out/$filename_no_ext-$percentage-out.csv
+        #     fi
+        # else
+        #     echo -n "$time," >> "$time_file"
+        # fi
+        ((index ++))
+    done
+    echo "" >> $time_file
 done
-
-printf '%s\n' "${capacities[@]}"
-
-# time_file="/workspace/results/capactiy-experiment/minimalloc-benchmarks/tvm-hillclimb/time/time.csv"
-# > $time_file
-
-# echo -e "\n\nRunning TVM-hillclimb for minimalloc-benchmarks (capacity experiment data)\n\n"
-
-# for percentage in "${percentages[@]}"; do  
-#     echo "Capacity=$percentage% of maximum load"
-#     for input in /workspace/benchmarks/minimalloc/*.csv; do
-#         base_filename=$(basename "$input")
-#         filename_no_ext="${base_filename%.*}"
-#         time=$(BASE_PATH=/workspace/results/capactiy-experiment/minimalloc-benchmarks/tvm-hillclimb TRACE_NAME=$filename_no_ext timeout 3m $bin $input hillclimb $capacity)
-#         ret=$?
-#         if [ $ret -eq 124 ]; then
-#             echo -n "Failed," >> $time_file
-#             if [ -f "/workspace/results/capactiy-experiment/minimalloc-benchmarks/tvm-hillclimb/csv-out/$filename_no_ext-out.csv" ]; then
-#                 rm /workspace/results/capactiy-experiment/minimalloc-benchmarks/tvm-hillclimb/csv-out/$filename_no_ext-out.csv
-#             fi
-#         else
-#             echo -n "$time," >> "$time_file"
-#         fi
-#     done
-#     echo "" >> $time_file
-# done
 
 # outfiles="/workspace/results/capactiy-experiment/minimalloc-benchmarks/tvm-hillclimb/csv-out"
 # makespan_file="/workspace/results/capactiy-experiment/minimalloc-benchmarks/tvm-hillclimb/makespan/makespan.txt"
