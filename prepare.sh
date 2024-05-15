2c0372127cc3d339c516a2de1ae5ff076a6a6b03
Panagiotis Xanthopoulos <panos0511@gmail.com>
2024-05-15 15:53:49 +0000

    No message

--- prepare.sh
+++ prepare.sh
@@ -21,83 +21,52 @@ mkdir /workspace/results/time-makespan
 mkdir /workspace/results/time-makespan/minimalloc-benchmarks
 mkdir /workspace/results/time-makespan/mindspore-benchmarks
 
-packers=("idealloc-r1" "idealloc-r21")
+benchmark_types=("minimalloc" "mindspore")
 
-for packer in "${packers[@]}"
+for benchmark_type in "${benchmark_types[@]}"
 do
-    mkdir /workspace/results/time-makespan/minimalloc-benchmarks/$packer
+    packers=("idealloc-r1" "idealloc-r21")
 
-    mkdir /workspace/results/time-makespan/minimalloc-benchmarks/$packer/time
-    mkdir /workspace/results/time-makespan/minimalloc-benchmarks/$packer/makespan
+    for packer in "${packers[@]}"
+    do
+        mkdir /workspace/results/time-makespan/$benchmark_type-benchmarks/$packer
+        mkdir /workspace/results/time-makespan/$benchmark_type-benchmarks/$packer/time
+        mkdir /workspace/results/time-makespan/$benchmark_type-benchmarks/$packer/makespan
+    done
 
-    mkdir /workspace/results/time-makespan/mindspore-benchmarks/$packer
+    packers=("minimalloc" "triton" "tflite-equality" "tflite-greedy-breadth" "tflite-greedy-inorder" "tflite-greedy-size" "tflite-mincostflow" "tflite-naive" "xla-best-fit-repacker" "xla-desc-size-heap-sim" "tvm-greedy-conflict" "tvm-greedy-size" "tvm-hillclimb" "mindspore")
 
-    mkdir /workspace/results/time-makespan/mindspore-benchmarks/$packer/time
-    mkdir /workspace/results/time-makespan/mindspore-benchmarks/$packer/makespan
-done
-
-packers=("minimalloc" "triton" "tflite-equality" "tflite-greedy-breadth" "tflite-greedy-inorder" "tflite-greedy-size" "tflite-mincostflow" "tflite-naive" "xla-best-fit-repacker" "xla-desc-size-heap-sim" "tvm-greedy-conflict" "tvm-greedy-size" "tvm-hillclimb" "mindspore")
-
-for packer in "${packers[@]}"
-do
-    mkdir /workspace/results/time-makespan/minimalloc-benchmarks/$packer
-
-    mkdir /workspace/results/time-makespan/minimalloc-benchmarks/$packer/time
-    mkdir /workspace/results/time-makespan/minimalloc-benchmarks/$packer/csv-out
-    mkdir /workspace/results/time-makespan/minimalloc-benchmarks/$packer/makespan
-
-    mkdir /workspace/results/time-makespan/mindspore-benchmarks/$packer
-
-    mkdir /workspace/results/time-makespan/mindspore-benchmarks/$packer/time
-    mkdir /workspace/results/time-makespan/mindspore-benchmarks/$packer/csv-out
-    mkdir /workspace/results/time-makespan/mindspore-benchmarks/$packer/makespan
+    for packer in "${packers[@]}"
+    do
+        mkdir /workspace/results/time-makespan/$benchmark_type-benchmarks/$packer
+        mkdir /workspace/results/time-makespan/$benchmark_type-benchmarks/$packer/time
+        mkdir /workspace/results/time-makespan/$benchmark_type-benchmarks/$packer/csv-out
+        mkdir /workspace/results/time-makespan/$benchmark_type-benchmarks/$packer/makespan
+    done
 done
 
 mkdir /workspace/results/capacity-experiment
 mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks
 mkdir /workspace/results/capacity-experiment/mindspore-benchmarks
 
-packers=("minimalloc" "tvm-hillclimb")
-
-for packer in "${packers[@]}"
+for benchmark_type in "${benchmark_types[@]}"
 do
-    mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks/$packer
+    packers=("minimalloc" "tvm-hillclimb")
 
-    mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks/$packer/time
-    
-    mkdir /workspace/results/capacity-experiment/mindspore-benchmarks/$packer
-
-    mkdir /workspace/results/capacity-experiment/mindspore-benchmarks/$packer/time
-
-    percs=("125" "150" "175" "200")
-
-    for perc in "${percs[@]}"
+    for packer in "${packers[@]}"
     do
-        mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks/$packer/$perc
-        mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks/$packer/$perc/csv-out
-        mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks/$packer/$perc/makespan
-
-        mkdir /workspace/results/capacity-experiment/mindspore-benchmarks/$packer/$perc
-        mkdir /workspace/results/capacity-experiment/mindspore-benchmarks/$packer/$perc/csv-out
-        mkdir /workspace/results/capacity-experiment/mindspore-benchmarks/$packer/$perc/makespan
+        mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks/$packer
+        mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks/$packer/time
+        mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks/$packer/csv-out
+        mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks/$packer/makespan
     done
-done
 
-mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks/idealloc
+    packers=("idealloc-r1" "idealloc-montecarlo-0" "idealloc-montecarlo-0.1" "idealloc-montecarlo-0.4")
 
-mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks/idealloc/time
-
-mkdir /workspace/results/capacity-experiment/mindspore-benchmarks/idealloc
-
-mkdir /workspace/results/capacity-experiment/mindspore-benchmarks/idealloc/time
-
-percs=("125" "150" "175" "200")
-
-for perc in "${percs[@]}"
-do
-    mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks/idealloc/$perc
-    mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks/idealloc/$perc/makespan
-
-    mkdir /workspace/results/capacity-experiment/mindspore-benchmarks/idealloc/$perc
-    mkdir /workspace/results/capacity-experiment/mindspore-benchmarks/idealloc/$perc/makespan
+    for packer in "${packers[@]}"
+    do
+        mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks/$packer
+        mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks/$packer/time
+        mkdir /workspace/results/capacity-experiment/minimalloc-benchmarks/$packer/makespan
+    done
 done
