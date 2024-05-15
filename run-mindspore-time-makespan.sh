b993eefd03a646dd35630501aef367dbe231f71e
Panagiotis Xanthopoulos <panos0511@gmail.com>
2024-05-15 13:48:30 +0000

    No message

---
+++ run-mindspore-time-makespan.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+adapt_bin="/workspace/idealloc/adapt"
+report_bin="/workspace/idealloc/report"
+bin="/workspace/mindspore-wrapper/bin/mindspore_packers"
+
+benchmark_types=("minimalloc" "mindspore")
+for benchmark_type in "${benchmark_types[@]}"; do
+    time_file="/workspace/results/time-makespan/$benchmark_type-benchmarks/mindspore/time/time.csv"
+    > $time_file
+
+    echo -e "\n\nRunning Mindspore for $benchmark_type-benchmarks\n\n"
+
+    for ((i=1; i<=20; i++)) do  
+        echo "$i out of 20 runs"
+        for input in /workspace/benchmarks/$benchmark_type/*.csv; do
+            base_filename=$(basename "$input")
+            filename_no_ext="${base_filename%.*}"
+            BASE_PATH=/workspace/results/time-makespan/$benchmark_type-benchmarks/mindspore TRACE_NAME=$filename_no_ext timeout 3m $bin $input 2>&1 > /workspace/mindspore-wrapper/output.txt
+            ret=$?
+            if [ $ret -eq 124 ]; then
+                echo -n "Failed," >> $time_file
+                if [ -f "/workspace/results/time-makespan/$benchmark_type-benchmarks/mindspore/csv-out/$filename_no_ext-out.csv" ]; then
+                    rm /workspace/results/time-makespan/$benchmark_type-benchmarks/mindspore/csv-out/$filename_no_ext-out.csv
+                fi
+            else
+                time=$(python3 /workspace/mindspore-wrapper/extract-time.py)
+                echo -n "$time," >> "$time_file"
+            fi
+        done
+        echo "" >> $time_file
+    done
+
+    outfiles="/workspace/results/time-makespan/$benchmark_type-benchmarks/mindspore/csv-out"
+    makespan_file="/workspace/results/time-makespan/$benchmark_type-benchmarks/mindspore/makespan/makespan.txt"
+    > $makespan_file
+
+    for file in $outfiles/*.csv; do
+        if [ -f "$file" ]; then
+            $adapt_bin $file
+            filename_no_ext=$(basename -- "$file" .csv)
+            output=$($report_bin "$filename_no_ext.plc" | grep Makespan | grep pages)
+            makespan=$(echo "$output" | awk '{print $NF}')
+            echo "$filename_no_ext: $makespan" >> "$makespan_file"
+            rm "$filename_no_ext.plc"
+            rm -rf '"'$filename_no_ext'"_m_62'
+        fi
+    done
+done
